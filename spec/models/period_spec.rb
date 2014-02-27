8b346df05ee1b69ba987568002be59c864a48a7b
UnSeulT <mathieu@carrandie.net>
2014-02-27 13:05:24 +0100

    Uniqueness of name

--- spec/models/period_spec.rb
+++ spec/models/period_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 describe Period do
-  subject { Period.new }
-  it 'can be persisted' do
-    subject.save.should be_true
+
+  describe 'Persistance' do
+    subject { FactoryGirl.build :period }
+    it 'can be persisted' do
+      subject.save.should be_true
+    end
   end
+
+  describe 'Unicity of name' do
+    before{ FactoryGirl.create :period, name:'Toto' }
+    subject { FactoryGirl.build :period, name:'Toto' }
+    it 'has a unique name' do
+      subject.save.should be_false
+    end
+  end
+
 end
