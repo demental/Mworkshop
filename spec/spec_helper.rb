1568ab82c77cf78b91e519852231b38682618bcb
arnaud sellenet <arnodmental@gmail.com>
2014-04-04 16:07:32 +0200

    focused pref

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,8 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
   config.include FactoryGirl::Syntax::Methods
+  config.filter_run focused: true
+  config.run_all_when_everything_filtered = true
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
