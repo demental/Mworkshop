cd39a5f5cacbc7cbe233c3ced7126672d3bb58c3
arnaud sellenet <arnodmental@gmail.com>
2014-04-04 16:28:34 +0200

    set factory assocations

--- spec/lib/wish_mill_spec.rb
+++ spec/lib/wish_mill_spec.rb
@@ -29,22 +29,22 @@ describe WishMill do
     let(:roller) { create(:workshop, grades: [boris.grade, andrej.grade], max_attendees: 2) }
     context 'when Boris and Andrej want to access a too small workshop' do
       before do
-        boris.wishes << build(:wish, period: first_period, workshop: boxing, weight: 2, wishgroup: 1)
-        boris.wishes << build(:wish, period: first_period, workshop: roller, weight: 1, wishgroup: 1)
+        boris.wishes << build(:wish, period: first_period, workshop: boxing, weight: 2, wish_group: 1)
+        boris.wishes << build(:wish, period: first_period, workshop: roller, weight: 1, wish_group: 1)
       end
 
       context 'when Boris was more happy before' do
         let(:boris_happiness) { 10 }
         let(:andrej_happiness) { 0 }
         it 'assigns Andrej to workshop' do
-          workshop.attendees.should eq [andrej]
+          boxing.attendees.should eq [andrej]
         end
       end
       context 'when Andrej was more happy before' do
         let(:boris_happiness) { 0 }
         let(:andrej_happiness) { 10 }
         it 'assigns Boris to workshop' do
-          workshop.attendees.should eq [boris]
+          boxing.attendees.should eq [boris]
         end
       end
     end
